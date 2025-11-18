600fb2598307f009fdb123d71dc24121632fabda
psj04011 <psj04011@gmail.com>
2025-11-18 09:09:19 +0900

    Delete main.py

--- main.py
+++
@@ -1,9 +0,0 @@
-import streamlit as st
-st.title( '나의 첫 웹 서비스 만들기')
-name=st.text_input('하하:')
-menu=st.selectbox('뭘봐?:',['미안...','꼽냐?'])
-if st.button('너 왜 웃니?'):
-  st.info(name+'와우!')
-  st.warning(menu+'ㅋㅋ?')
-      
-  st.balloons()
